480314c33a2fd8bd6edb69b23f7ffc12d573af05
Brendan Cullen <brendancullen996@gmail.com>
2023-06-07 10:00:40 -0400

    fix warning about maplibre css import

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
